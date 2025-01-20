683167a29ca5004fbdb65d8f7e53000f34f7766b
evertonc15 <everton.ci@outlook.com>
2025-01-20 10:48:54 -0300

    first commit

---
+++ src/test/java/SpringBootCourse/evertonc15/com/github/produtosapi/ProdutosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package SpringBootCourse.evertonc15.com.github.produtosapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdutosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
